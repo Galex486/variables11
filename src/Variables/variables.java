5a1782708665c23e35bf017ff6b8c001eb31b6a9
Galex486 <93607020+Galex486@users.noreply.github.com>
2023-09-29 02:27:29 +0300

    Update variables.java

--- src/Variables/variables.java
+++ src/Variables/variables.java
@@ -1,2 +1,30 @@
-package Variables;public class variables {
+package Variables;
+
+public class variables {
+    public long CurrentWorldPopulation = 7811080815L;
+    public int CarsProducedThisYear = 55235358;
+    public char FirstLetterInAlphabet = 'A';
+    public boolean JavaIsAmazing = 10 = 10;
+    public double Pi = 3,14159;
+
+    public long getPopulation() {
+        return CurrentWorldPopulation;
+    }
+
+    public char getYear() {
+        return CarsProducedThisYear;
+    }
+
+    public int getLetter() {
+        return  FirstLetterInAlphabet;
+    }
+
+    public boolean getJava() {
+        return JavaIsAmazing;
+    }
+
+    public double getPi() {
+        return Pi;
+    }
+
 }
