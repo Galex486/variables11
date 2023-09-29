ffa2d13005730aea6c221aa3dc113d2a76c6b009
Galex486 <93607020+Galex486@users.noreply.github.com>
2023-09-29 10:11:42 +0300

    Update variables.java

--- src/Variables/variables.java
+++ src/Variables/variables.java
@@ -1,30 +1,11 @@
-package Variables;
+package variables;
 
-public class variables {
+public class Variables {
     public long CurrentWorldPopulation = 7811080815L;
     public int CarsProducedThisYear = 55235358;
     public char FirstLetterInAlphabet = 'A';
-    public boolean JavaIsAmazing = 10 = 10;
-    public double Pi = 3,14159;
+    public boolean JavaIsAmazing = true;
+    public double Pi = 3.14159;
 
-    public long getPopulation() {
-        return CurrentWorldPopulation;
-    }
-
-    public char getYear() {
-        return CarsProducedThisYear;
-    }
-
-    public int getLetter() {
-        return  FirstLetterInAlphabet;
-    }
-
-    public boolean getJava() {
-        return JavaIsAmazing;
-    }
-
-    public double getPi() {
-        return Pi;
-    }
 
 }
