b18cc047217c2693b4834b3230d3582bdae4aba5
Galex486 <93607020+Galex486@users.noreply.github.com>
2023-09-29 10:10:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import variables.Variables;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("a. Current World Population:" +7811080815L);
+        System.out.println("b. Cars Produced This Year:" +55235358);
+        System.out.println("c. First Letter In Alphabet:" +"A");
+        System.out.println("d. Java Is Amazing:" +true);
+        System.out.println("e. Pi:" +3.14159);
+
     }
 }
